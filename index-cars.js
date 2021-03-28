d43ef69b66dd9c767e0fc41611c37688c090d69f
= <=>
2021-03-28 07:38:49 +0100

    updated car lot

--- index-cars.js
+++ index-cars.js
@@ -1,13 +1,15 @@
 const vehicleMod = require('./models/vehicle');
 const writerLib = require("./lib/writer");
-
-let writer = writerLib.writer();
+const writer = writerLib.writer();
 
 var carLot = [];
 carLot.push(new vehicleMod.vehicle("Ford", "Fiesta", 200));
 carLot.push(new vehicleMod.vehicle("Ford", "Focus", 400));
 carLot.push(new vehicleMod.vehicle("Ford", "Estate", 600));
 
-let vehicleStr = JSON.stringify(carLot, null, 2);
+var carLotObj = {};
+carLotObj.carlot = carLot;
+
+let vehicleStr = JSON.stringify(carLotObj, null, 2);
 
 writer.writeToFile("./output/car.json", vehicleStr);
